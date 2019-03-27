258824fe7887716a0bd938b52834bce7158ef8bf
damienzonly <damianomails@gmail.com>
2019-03-27 10:09:51 +0100

    Control migration into App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,60 @@ import React, { Component } from "react";
 import Display from "./components/Display";
 import Navbar from "./components/static/Navbar";
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [],
+            subTarget: "",
+            msgIndex: 0
+        };
+    }
+    targetChange = event => {
+        this.setState({ subTarget: event.target.value });
+    };
+    sub = event => {
+        event.preventDefault();
+        console.log("Subbed to:", this.state.subTarget);
+        this.setState({ subTarget: "" });
+    };
+    keyPress = event => {
+        if (event.target.value === "") return;
+        if (event.keyCode === 13) {
+            this.sub(event);
+        }
+    };
+    pushMessage = message => {
+        if (message === "") return;
+        this.setState(state => {
+            let newMessage = {
+                index: state.msgIndex + 1,
+                message
+            }
+            if (state.messages.length === 10) {
+                return {
+                    msgIndex: state.msgIndex + 1,
+                    messages: [
+                        ...state.messages.slice(1),
+                        newMessage
+                    ]
+                }
+            } else {
+                return {
+                    msgIndex: state.msgIndex + 1,
+                    messages: [
+                        ...state.messages,
+                        newMessage
+                    ]
+                }
+            }
+        })
+    };
     render() {
         return (
             <div className="row no-gutters">
                 <div className="col-md-4 offset-md-4">
                     <Navbar />
-                    <Display />
+                    <Display messages={this.state.messages} subTarget={this.state.subTarget} pushMessage={this.pushMessage} targetChange={this.targetChange} keyPress={this.keyPress} />
                 </div>
             </div>
         )
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,57 +1,8 @@
 import React, { Component } from "react";
 import ChatWindow from "./ChatWindow";
 export default class Display extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-            subTarget: "",
-            msgIndex: 0
-        };
-    }
-    targetChange = e => {
-        this.setState({ subTarget: e.target.value });
-    };
-    sub = event => {
-        event.preventDefault();
-        console.log("Subbed to:", this.state.subTarget);
-        this.setState({ subTarget: "" });
-    };
-    keyPress = event => {
-        if (event.target.value === "") return;
-        if (event.keyCode === 13) {
-            this.sub(event);
-        }
-    };
-    pushMessage = message => {
-        if (message === "") return;
-        let messagesCopy = [...this.state.messages];
-        if (this.state.messages.length === 0) {
-            this.setState(state => {
-                return {
-                    msgIndex: state.msgIndex + 1,
-                    messages: [{ index: state.msgIndex + 1, message }]
-                };
-            });
-            return;
-        } else if (this.state.messages.length === 10) {
-            messagesCopy.splice(1);
-        }
-        this.setState(state => {
-            return {
-                msgIndex: state.msgIndex + 1,
-                messages: [
-                    ...messagesCopy,
-                    {
-                        index: state.msgIndex + 1,
-                        message
-                    }
-                ]
-            };
-        });
-    };
     render() {
-        let items = this.state.messages.map(message => (
+        let items = this.props.messages.map(message => (
             <li className="list-group-item text-right border-0" key={message.index}>
                 {message.message}
             </li>
@@ -60,13 +11,13 @@ export default class Display extends Component {
             <>
                 <div className="form-inline">
                     <div className="form-group w-100">
-                        <input type="text" onChange={this.targetChange} onKeyDown={this.keyPress} placeholder="Sub to user" className="form-control w-100" value={this.state.subTarget} />
+                        <input type="text" value={this.props.subTarget} onChange={this.props.targetChange} onKeyDown={this.props.keyPress} placeholder="Sub to user" className="form-control w-100"/>
                     </div>
                 </div>
 
                 <div key="display" className="">
                     <ul className="list-group">{items}</ul>
-                    <ChatWindow pm={this.pushMessage} />
+                    <ChatWindow pm={this.props.pushMessage} />
                 </div>
             </>
         );
