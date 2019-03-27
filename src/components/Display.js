cc6b65c37597b6f1ed38d8f531a403b17f75f90e
damienzonly <damianomails@gmail.com>
2019-03-27 10:15:32 +0100

    Display.js as Functional Component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
 import ChatWindow from "./ChatWindow";
-export default class Display extends Component {
-    render() {
-        let items = this.props.messages.map(message => (
-            <li className="list-group-item text-right border-0" key={message.index}>
-                {message.message}
-            </li>
-        ));
-        return (
-            <>
-                <div className="form-inline">
-                    <div className="form-group w-100">
-                        <input type="text" value={this.props.subTarget} onChange={this.props.targetChange} onKeyDown={this.props.keyPress} placeholder="Sub to user" className="form-control w-100"/>
-                    </div>
-                </div>
+import React from 'react'
 
-                <div key="display" className="">
-                    <ul className="list-group">{items}</ul>
-                    <ChatWindow pm={this.props.pushMessage} />
+export default function Display(props) {
+    let items = props.messages.map(message => (
+        <li className="list-group-item text-right border-0" key={message.index}>
+            {message.message}
+        </li>
+    ));
+    return (
+        <>
+            <div className="form-inline">
+                <div className="form-group w-100">
+                    <input type="text" value={props.subTarget} onChange={props.targetChange} onKeyDown={props.keyPress} placeholder="Sub to user" className="form-control w-100"/>
                 </div>
-            </>
-        );
-    }
+            </div>
+    
+            <div key="display" className="">
+                <ul className="list-group">{items}</ul>
+                <ChatWindow pm={props.pushMessage} />
+            </div>
+        </>
+    );
 }
