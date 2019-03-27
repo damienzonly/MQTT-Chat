891a2d870d8ca35ee020d30fcd25dfa25c97f8b4
damiano <damianomails@gmail.com>
2019-03-27 23:30:08 +0100

    sender and recipient fields
    now correctly implemented the way messages are sent and received

--- src/App.js
+++ src/App.js
@@ -1,64 +1,94 @@
 import React, { Component } from "react";
 import Display from "./components/Display";
 import Navbar from "./components/static/Navbar";
+
+let mqtt = require("mqtt");
+
 class App extends Component {
     constructor(props) {
         super(props);
+        this.client = mqtt.connect("ws://localhost", {
+            port: 8888,
+            clientId: "mqtt-chat-user-" + Math.floor(Math.random() * 1000)
+        });
+        this.client
+            .on("connect", () => {
+                console.debug("Client mqtt connected");
+            })
+            .on("error", err => {
+                if (err) console.error(err);
+            })
+            .on("message", (topic, message) => {
+                this.pushMessage(message.toString(), "text-left");
+            });
         this.state = {
             messages: [],
             subTarget: "",
+            subSelf: "",
             msgIndex: 0
         };
     }
-    targetChange = event => {
+    subTargetChange = event => {
         this.setState({ subTarget: event.target.value });
     };
-    sub = event => {
-        event.preventDefault();
-        console.log("Subbed to:", this.state.subTarget);
-        this.setState({ subTarget: "" });
+    subSelfChange = event => {
+        this.setState({ subSelf: event.target.value });
     };
-    keyPress = event => {
+    subTargetKeyPress = event => {
         if (event.target.value === "") return;
         if (event.keyCode === 13) {
-            this.sub(event);
+            this.subTargetChange(event);
         }
     };
-    pushMessage = message => {
+    subSelfKeyPress = event => {
+        if (event.target.value === "") return;
+        if (event.keyCode === 13) {
+            this.subSelfChange(event);
+            this.setState(state => {
+                this.client.subscribe(state.subSelf);
+            });
+        }
+    };
+    pushMessage = (message, side) => {
         if (message === "") return;
         this.setState(state => {
+            this.client.publish(state.subTarget, message);
             let newMessage = {
                 index: state.msgIndex + 1,
-                message
-            }
+                message,
+                side: side || "text-right"
+            };
             if (state.messages.length === 10) {
                 return {
                     msgIndex: state.msgIndex + 1,
-                    messages: [
-                        ...state.messages.slice(1),
-                        newMessage
-                    ]
-                }
+                    messages: [...state.messages.slice(1), newMessage]
+                };
             } else {
                 return {
                     msgIndex: state.msgIndex + 1,
-                    messages: [
-                        ...state.messages,
-                        newMessage
-                    ]
-                }
+                    messages: [...state.messages, newMessage]
+                };
             }
-        })
+        });
     };
     render() {
         return (
             <div className="row no-gutters">
                 <div className="col-md-4 offset-md-4">
                     <Navbar />
-                    <Display messages={this.state.messages} subTarget={this.state.subTarget} pushMessage={this.pushMessage} targetChange={this.targetChange} keyPress={this.keyPress} />
+                    <Display
+                        messages={this.state.messages}
+                        subTarget={this.state.subTarget}
+                        subSelf={this.state.subSelf}
+                        pushMessage={this.pushMessage}
+                        subTargetChange={this.subTargetChange}
+                        subSelfChange={this.subSelfChange}
+                        subSelfKeyPress={this.subSelfKeyPress}
+                        subTargetKeyPress={this.subTargetKeyPress}
+                    />
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,20 +1,42 @@
 import ChatWindow from "./ChatWindow";
-import React from 'react'
+import React from "react";
 
 export default function Display(props) {
-    let items = props.messages.map(message => (
-        <li className="list-group-item text-right border-0" key={message.index}>
-            {message.message}
-        </li>
-    ));
+    let items = props.messages.map(message => {
+        let cls = "list-group-item border-0 " + message.side;
+        return (
+            <li className={cls} key={message.index}>
+                {message.message}
+            </li>
+        );
+    });
     return (
         <>
+            <small>Enter channels and press Enter</small>
             <div className="form-inline">
                 <div className="form-group w-100">
-                    <input type="text" value={props.subTarget} onChange={props.targetChange} onKeyDown={props.keyPress} placeholder="Sub to user" className="form-control w-100"/>
+                    <input
+                        type="text"
+                        value={props.subSelf}
+                        onChange={props.subSelfChange}
+                        onKeyDown={props.subSelfKeyPress}
+                        placeholder="Self channel"
+                        className="form-control w-100"
+                    />
                 </div>
             </div>
-    
+            <div className="form-inline">
+                <div className="form-group w-100 mt-2">
+                    <input
+                        type="text"
+                        value={props.subTarget}
+                        onChange={props.subTargetChange}
+                        onKeyDown={props.subTargetKeyPress}
+                        placeholder="Target channel"
+                        className="form-control w-100"
+                    />
+                </div>
+            </div>
             <div key="display" className="">
                 <ul className="list-group">{items}</ul>
                 <ChatWindow pm={props.pushMessage} />
