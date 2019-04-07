d6392aeaf5c42bc78a40ece51b5bf095e713c89d
damiano <damianomails@gmail.com>
2019-04-08 01:07:50 +0200

    Introducing String.prototype.capFirst

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,12 @@ import React from "react";
 
 export default function Display(props) {
     if (props.currentRoom) {
-        return <div className="text-center">{props.currentRoom}</div>
+        return (
+            <div className="container">
+                <div className="container">{props.currentRoom.capFirst()}</div>
+                <InputArea />
+            </div>
+        );
     }
-    return <div className="mt-4 text-center">Select one room using the left sidebar</div>
+    return <div className="mt-4 text-center">Select one room using the left sidebar</div>;
 }
