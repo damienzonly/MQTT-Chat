ef71bcec404e40829e32bfd5a531e2e15ffaf869
damiano <damianomails@gmail.com>
2019-04-08 02:13:47 +0200

    Introduced third column

---
+++ src/components/OnlineStatus.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class OnlineStatus extends Component {
+    render() {
+        return <div className="p-1 h3 text-center">Online status</div>;
+    }
+}
